c6440387581e1c030c915d64f54f5a58c4dfdd56
MD SOHAIL ALI <90498286+sohaill123@users.noreply.github.com>
2023-06-13 21:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	 const alphanumericString = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+  let left = 0;
+  let right = alphanumericString.length - 1;
+
+  while (left < right) {
+    if (alphanumericString[left] !== alphanumericString[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 
 }
 module.exports = palindrome
